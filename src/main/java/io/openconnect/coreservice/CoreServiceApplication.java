9cf7181a138cbe35a3e8a77825aca73251947b10
Charles Queiroz <charles.queiroz@dazen.com.br>
2022-12-18 18:15:38 -0300

    Merge da Develop na Main (#8)
    Initial Setup first iteration

--- src/main/java/io/openconnect/coreservice/CoreServiceApplication.java
+++ src/main/java/io/openconnect/coreservice/CoreServiceApplication.java
@@ -1,6 +1,5 @@
 package io.openconnect.coreservice;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import static org.springframework.boot.SpringApplication.*;
