bc27c2e5f7f09db40d147fd1929b32f38fe3d976
Renato Queiroz <renato.queiroz@dazen.com.br>
2023-01-09 08:53:36 -0300

    OPC-62 Implementado teste de api

--- src/main/java/io/openconnect/coreservice/controller/AppInfoController.java
+++ src/main/java/io/openconnect/coreservice/controller/AppInfoController.java
@@ -1,20 +1,19 @@
 package io.openconnect.coreservice.controller;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("version")
+@RequestMapping("/api")
 public class AppInfoController {
     @Value("${application-version}")
     private String appInfo;
 
     @GetMapping
-    public ResponseEntity<String> getAppVersion(){
-        return new ResponseEntity<>(appInfo, HttpStatus.OK);
+    public @ResponseBody String getAppVersion() {
+        return appInfo;
     }
 }
